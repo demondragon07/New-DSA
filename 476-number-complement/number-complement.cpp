a9f1779dd5b01d414454693fa395fe0e9f2fa052
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-07-10 23:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (15.73%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int findComplement(int num) {
         int numbits=(int)log2(num)+1;
-
-        for(int i=0;i<numbits;i++){
-            num=num^(1<<i);
-        }
-       return num; 
+        unsigned int mask=(1U<<numbits)-1;
+        
+       return num^mask; 
     }
 };
