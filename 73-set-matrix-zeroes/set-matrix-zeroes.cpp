4969679b785642674da1fd957b5e3214569691a1
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-07-27 13:10:16 +0530

    Time: 1 ms (15.65%) | Memory: 18.4 MB (95.79%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,38 +3,54 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int>rows(n,0);
-        vector<int>columns(m,0);
-        for(int i=0;i<n;i++){
+
+        bool isfirstrowimpacted=false;
+        bool isfirstcolumnimpacted=false;
+
+        for(int i=0;i<1;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    columns[j]=1;
-                }
+              if(matrix[i][j]==0){
+               isfirstrowimpacted=true;
+               break;
+              } 
             }
         }
 
-        for(int j=0;j<m;j++){
-            for(int i=0;i<n;i++){
-                if(matrix[i][j]==0){
-                    rows[i]=1;
-                }
-            }
-        }
-
+       for(int j=0;j<1;j++){
         for(int i=0;i<n;i++){
-            if(rows[i]==1){
-                for(int j=0;j<m;j++){
-                    matrix[i][j]=0;
-                }
+            if(matrix[i][j]==0){
+            isfirstcolumnimpacted=true;
+            break;
             }
         }
+       } 
+         
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+              if(matrix[i][j]==0){
+                 matrix[i][0]=0;
+                 matrix[0][j]=0;
+              }
+            }
+        }
+       for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(matrix[i][0]==0 || matrix[0][j]==0){
+                matrix[i][j]=0;
+            }
+        }
+       } 
 
+      if(isfirstrowimpacted){
         for(int j=0;j<m;j++){
-            if(columns[j]==1){
-                for(int i=0;i<n;i++){
-                    matrix[i][j]=0;
-                }
-            }
+            matrix[0][j]=0;
         }
+      } 
+
+      if(isfirstcolumnimpacted){
+        for(int i=0;i<n;i++){
+            matrix[i][0]=0;
+        }
+      }
     }
 };
