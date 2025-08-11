743923b5a98f928d0d20acf3e271324a1adee3fd
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-08-11 15:19:07 +0530

    Time: 160 ms (8.69%) | Memory: 49 MB (8.00%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,28 @@
+class MyCalendar {
+  
+public:
+ map<int,int>mp;
+    MyCalendar() {
+        
+    }
+    bool book(int startTime, int endTime) {
+        mp[startTime]++;
+        mp[endTime]--;
+        int sum=0;
+        for(auto x:mp){
+            sum+=x.second;
+            if(sum>1){
+                mp[startTime]--;
+                mp[endTime]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
