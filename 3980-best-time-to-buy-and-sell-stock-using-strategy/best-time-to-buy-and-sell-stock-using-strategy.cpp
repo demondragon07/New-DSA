7e66830a91b1f378b76b26323ad904d5d0a12f2c
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-08-17 23:34:41 +0530

    Time: 8 ms (86.60%) | Memory: 302.9 MB (76.51%) - LeetSync

---
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,26 @@
+#define ll long long 
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+       ll n=strategy.size();
+       ll base=0;
+       for(ll i=0;i<n;i++){
+           base+=prices[i]*strategy[i];
+       }
+     
+     ll ans=base;
+     ll slide=0;
+     for(int i=0;i<k;i++) {base-=prices[i]*strategy[i];}
+     for(int i=k/2;i<k;i++){slide+=prices[i];}
+     ans=max(ans,base+slide);
+    for(int i=k;i<n;i++){
+        base-=prices[i]*strategy[i];
+        base+=prices[i-k]*strategy[i-k];
+        slide-=prices[i-k/2];
+        slide+=prices[i];
+        ans=max(ans,slide+base);
+    }
+
+  return ans;
+    }
+};
