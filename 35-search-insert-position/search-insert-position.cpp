2499e4a146bdf0597dfe760d3ded8d0996f02a8e
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-08-22 02:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (76.67%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size();
         int s=0;
         int e=n-1;
-        int ans=0;
          while(s<=e){
             int mid=s+(e-s)/2;
             if(nums[mid]==target){
