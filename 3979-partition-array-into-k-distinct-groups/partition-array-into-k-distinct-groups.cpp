221e12603b51a1e90ed4ebc8270fe3cf586d1a95
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-08-25 10:49:47 +0530

    Time: 87 ms (12.50%) | Memory: 272.3 MB (25.00%) - LeetSync

---
+++ 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+         int n=nums.size();
+         if(n%k!=0) return false;
+         map<int,int>mp;
+         for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]]>n/k) return false;
+         }
+         return true;
+    }
+};
