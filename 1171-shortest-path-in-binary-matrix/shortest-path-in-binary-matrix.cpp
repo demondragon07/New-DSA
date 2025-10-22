c58c733c5c60d69f02d22d02ed8fe4380f934e63
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-10-22 08:22:54 +0530

    Time: 287 ms (5.04%) | Memory: 83 MB (5.15%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-       vector<vector<int>>directions={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,-1},{-1,1}};
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        int level=0;
-        if(grid[0][0]!=0) return -1;
-        grid[0][0]=1;
+        typedef pair<int,pair<int,int>> p;
+        vector<vector<int>>directions={{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1}};
+        vector<vector<int>>result(n,vector<int>(n,INT_MAX));
+        priority_queue<p,vector<p>,greater<p>>pq;
         auto isSafe=[&](int x,int y){
-           return x>=0 && x<n && y>=0 && y<n;
+            return x>=0 && x<n && y>=0 && y<n;
         };
-        while(!q.empty()){
-            int m=q.size();
-            while(m--){
-           pair<int,int>p=q.front();
-           int x=p.first;
-           int y=p.second;
-           q.pop();
-           if(x==n-1 && y==n-1)return level+1;
-           for(auto dir:directions){
-              int x_=x+dir[0];
-              int y_=y+dir[1];
-              if(isSafe(x_,y_) && grid[x_][y_]==0){
-                q.push({x_,y_});
-                grid[x_][y_]=1;
-              }
-           }
-         }
-         level++;
-        } 
-        return -1;
+        if(grid[0][0]!=0)return -1;
+        pq.push({0,{0,0}});
+        result[0][0]=0;
+        while(!pq.empty()){
+            auto p1=pq.top();
+            int d=p1.first;
+            int x=p1.second.first;
+            int y=p1.second.second;
+            pq.pop();
+            for(auto dir:directions){
+                int x_=x+dir[0];
+                int y_=y+dir[1];
+                if(isSafe(x_,y_) && grid[x_][y_]==0 && d+1<result[x_][y_]){
+                    result[x_][y_]=d+1;
+                    pq.push({d+1,{x_,y_}});
+                }
+            }
+        }
+        if(result[n-1][n-1]==INT_MAX) return -1;
+        return result[n-1][n-1]+1; 
     }
 };
