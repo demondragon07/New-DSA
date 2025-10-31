b97709954bbd447fe1c9cc3dac55712d070dff7a
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-11-01 02:23:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (73.03%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        vector<int>dp(101,0);
-        int n=nums.size();
-        //dp[i]=max_steal a person can do till house [i];
-        dp[0]=0;
-        dp[1]=nums[0];
-        for(int i=2;i<=n;i++){
-            int take=nums[i-1]+dp[i-2];
-            int skip=dp[i-1];
-           dp[i]=max(take,skip);    
-        }   
-       return dp[n];
+    int rob(vector<int>& nums){
+     vector<int>t(101,0);
+       int n=nums.size();
+     //t[i]=Max profit till ith house;
+       int prevkaprev=0;
+       int prev=nums[0];
+     for(int i=2;i<=n;i++){
+        int steal=nums[i-1]+prevkaprev;
+        int skip=prev;
+        int curr=max(steal,skip);
+        prevkaprev=prev;
+        prev=curr;
+     }
+      return prev;
     }
 };
