9b61d9039f1df3b09fb2e215fa3aac7b57de2a1b
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-11-01 17:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     int rob(vector<int>& nums){
         int n=nums.size();
-        vector<int>dp(101,0);
         if(n==0) return 0;
         if(n==1) return nums[0];
-        dp[0]=0;
-        // if 1st house is taken;
-        //dp[i] represents max profit till ith house
+       int prevkaprev=0;  
+       int prev=0;      
+       // if 1st house is taken;
         for(int i=1;i<=n-1;i++){
-            int steal=nums[i-1]+ ((i-2>=0)?dp[i-2]:0);
-            int skip=dp[i-1];
-            dp[i]=max(steal,skip);
+            int steal=nums[i-1]+ prevkaprev;
+            int skip=prev;
+            int curr=max(steal,skip);
+            prevkaprev=prev;
+            prev=curr;
         }
-       int result1=dp[n-1];
+       int result1=prev;
 
-       dp.clear();
        // if 1st house is not taken
-       //dp[i] represent the max profit till ith house
-       dp[0]=0;
-       dp[1]=0;
+      int prevkaprev2=0;
+      int prev2=0;
        for(int i=2;i<=n;i++){
-          int steal=nums[i-1]+((i-2>=0)?dp[i-2]:0);
-          int skip=dp[i-1];
-          dp[i]=max(steal,skip);
+          int steal=nums[i-1]+prevkaprev2;
+          int skip=prev2;
+          int curr2=max(steal,skip);
+          prevkaprev2=prev2;
+          prev2=curr2;
        }
-       int result2=dp[n];
+       int result2=prev2;
        return max(result1,result2);
     }
 };
