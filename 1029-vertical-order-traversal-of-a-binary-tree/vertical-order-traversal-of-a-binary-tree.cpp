0a75e26e5cfa7b9ee146f71f6525e36253120d0d
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-12-23 20:49:38 +0530

    Time: 3 ms (39.38%) | Memory: 16.6 MB (15.14%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,45 +11,33 @@
  */
 class Solution {
 public:
-  void solve(TreeNode*root ,vector<pair<int,pair<int,int>>>&p){
-       auto u=p.back(); 
-       if(root==NULL)return;    
-       if(root->left!=NULL){
-         p.push_back({root->left->val,{u.second.first+1,u.second.second-1}});
-         solve(root->left,p);
-       }
-       if(root->right!=NULL){
-        p.push_back({root->right->val,{u.second.first+1,u.second.second+1}});
-        solve(root->right,p);
-       }
-   }
-   static bool comp(pair<int,pair<int,int>>&a ,pair<int,pair<int,int>>&b){
-        if(a.second.second==b.second.second && (a.second.first==b.second.first)) return a.first<b.first;
-        else if(a.second.second==b.second.second && (a.second.first!=b.second.first)) return a.second.first<b.second.first;
-        else return a.second<b.second;
-   }
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<pair<int,pair<int,int>>>p;
+        map<int,map<int,multiset<int>>>mp;
         if(root==NULL)return {};
-        p.push_back({root->val,{0,0}});
-        solve(root,p);
-        sort(p.begin(),p.end(),comp);
-        int minidx=INT_MAX;
-        int maxidx=INT_MIN;
-        for(auto x:p){
-            minidx=min(minidx,x.second.second);
-            maxidx=max(maxidx,x.second.second);
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+            auto u=q.front();
+            mp[u.second.first][u.second.second].insert(u.first->val);
+            q.pop();
+            if(u.first->left!=NULL){
+                q.push({u.first->left,{u.second.first-1,u.second.second+1}});
+            }
+            if(u.first->right!=NULL){
+                q.push({u.first->right,{u.second.first+1,u.second.second+1}});
+            }
+         }
+        } 
+        vector<vector<int>>ans;
+        for(auto x:mp){
+            vector<int>level;
+          for(auto u:x.second){
+            level.insert(level.end(),u.second.begin(),u.second.end());
+          }
+          ans.push_back(level);
         }
-       int sz=maxidx-minidx+1;
-       vector<vector<int>>ans(sz);
-       for(auto x:p){
-        int val=x.first;
-        int oldidx=x.second.second;
-        int shidx=oldidx-minidx;
-        ans[shidx].push_back(val);
-       }
-
-       return ans;
+        return ans;
     }
 };
