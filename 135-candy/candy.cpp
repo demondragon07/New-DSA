8172d820db7bfc97b33e4ab6d71bbc6071462fd1
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2025-12-27 17:00:11 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (84.89%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int>candies(n,1);
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                candies[i]=candies[i-1]+1;
-            }
+        int i=1;
+        int ans=1;
+        while(i<n){
+              if(ratings[i]==ratings[i-1]){
+                ans+=1;
+                i++;
+              }
+              int peak=1;
+              while(i<n && ratings[i]>ratings[i-1]){
+                peak+=1;
+                ans+=peak;
+                i++;
+              }
+              int down=1;
+              while(i<n && ratings[i]<ratings[i-1]){
+                 ans+=down;
+                 down++;
+                 i++;
+              }
+              if(down>peak)ans+=down-peak;
         }
-
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                candies[i]=max(candies[i],candies[i+1]+1);
-            }
-        }
-        int ans=accumulate(candies.begin(),candies.end(),0);
         return ans;
     }
 };
