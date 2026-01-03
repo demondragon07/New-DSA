8d992f40d8cc330c0e7a042f144bc89c0f3285a6
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-01-03 09:24:50 +0530

    Time: 6 ms (23.63%) | Memory: 9.7 MB (47.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int>mp1;
-        map<char,int>mp2;
-        if(s.size()!=t.size())return false;
-        int m=s.size();
-        for(int i=0;i<m;i++){
-            char ch1=s[i];
-            char ch2=t[i];
-            mp1[ch1]++;
-            mp2[ch2]++;
-        }
-    return mp1==mp2;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
