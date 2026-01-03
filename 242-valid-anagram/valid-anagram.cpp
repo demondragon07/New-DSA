05d5d5ab4ab3d507b13acc2cc9a9586ffc6f5b18
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-01-03 09:31:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.length()!=t.length())return false;
+        vector<int>arr(26,0);
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch1=s[i];
+            char ch2=t[i];
+            arr[ch1-'a']++;
+            arr[ch2-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0)return false;
+        }
+        return true;
     }
 };
