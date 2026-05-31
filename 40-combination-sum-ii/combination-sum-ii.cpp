db0c3316dccb5f9eb6c44a85b1f7d28c5d0fa261
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-05-31 15:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (7.74%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void findcombination(vector<vector<int>>&ans, vector<int>&ds, int idx, int target,vector<int>arr){
+        if(target==0)ans.push_back(ds);
+        for(int i=idx;i<arr.size();i++){
+            if(i>idx && arr[i]==arr[i-1])continue;
+            if(arr[i]>target) return;
+            ds.push_back(arr[i]);
+            findcombination(ans,ds,i+1,target-arr[i],arr);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(candidates.begin(),candidates.end());
+        findcombination(ans,ds,0,target,candidates);
+        return ans;
+
+    }
+};
