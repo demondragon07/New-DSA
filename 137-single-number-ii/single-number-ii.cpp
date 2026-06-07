680948ed720f9c3cb6c57d1e7f1f53bd9a08b21d
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-06-07 17:26:05 +0530

    Time: 3 ms (30.49%) | Memory: 13.4 MB (34.36%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result=0;
-        for(int k=0;k<=31;k++){
-            int temp=(1<<k);
-            int countzeros=0;
-            int countones=0;
-            for(auto num:nums){
-             if(num&temp)countones++;
-             else countzeros++;
-            }
-          if(countones%3!=0){
-            result=result|temp;
-          }
+        int res=0;
+        for(int i=0;i<32;i++){ 
+            int ones=0;
+            int zero=0; 
+           for(auto num:nums){
+              if(num & (1<<i))ones++;
+              else zero++;
+           }
+           if(ones%3)res=(res|(1<<i));
         }
-        return result;
+        return res;
     }
 };
