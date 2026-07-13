e320172f95bddab2fc9a05d1b74862898e880273
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-07-13 11:59:14 +0530

    Time: 10 ms (26.14%) | Memory: 15.7 MB (57.67%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,17 +1,54 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        long long n=nums.size();
+    long long sumofnge(vector<int>&nums){
+        int n=nums.size();
+        vector<int>nge(n);
+        vector<int>ngpee(n);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i])st.pop();
+            ngpee[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i])st.pop();
+            nge[i]=st.empty()?n:st.top();
+            st.push(i);
+        }
         long long total=0;
-        for(long long i=0;i<n;i++){
-            int  maxii=nums[i];
-            int minii=nums[i];
-            for(long long j=i;j<n;j++){
-                maxii=max(maxii,nums[j]);
-                minii=min(minii,nums[j]);
-                total+=maxii-minii;
-            }
+        for(int i=0;i<n;i++){
+            int left=i-ngpee[i];
+            int right=nge[i]-i;
+            total+=(left*right*1LL*nums[i]);
         }
         return total;
+    }
+    long long sumofnse(vector<int>&nums){
+        int n=nums.size();
+        vector<int>nse(n);
+        vector<int>nspee(n);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]>nums[i])st.pop();
+            nspee[i]=st.empty()?-1:st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>=nums[i])st.pop();
+            nse[i]=st.empty()?n:st.top();
+            st.push(i);
+        }
+        long long total=0;
+        for(int i=0;i<n;i++){
+            int left=i-nspee[i];
+            int right=nse[i]-i;
+            total+=(left*right*1LL*nums[i]);
+        }
+        return total;
+    }
+    long long subArrayRanges(vector<int>& nums) {
+     return sumofnge(nums)-sumofnse(nums); 
     }
 };
