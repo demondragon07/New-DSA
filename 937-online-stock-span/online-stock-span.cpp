5d83fd55979fccbb0243425a70dad808240f17c0
demondragon07 <127862678+demondragon07@users.noreply.github.com>
2026-07-19 11:13:31 +0530

    Time: 525 ms (5.16%) | Memory: 92.8 MB (17.65%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.cpp
@@ -0,0 +1,23 @@
+class StockSpanner {
+public:
+   vector<int>arr;
+    StockSpanner() {
+    }
+    
+    int next(int price) {
+        int cnt=1;
+        int n=arr.size();
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]<=price)cnt++;
+            else break;
+        }
+        arr.push_back(price);
+        return cnt;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
